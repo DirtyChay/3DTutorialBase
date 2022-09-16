34242b0d8ab67bbfc1c6a7be39db4160fe6ec9cb
Chayan Tronson <chayan995@gmail.com>
2022-09-15 22:10:08 -0700

    supernova implemented

--- Assets/Scripts/Player/Abilities/SuperNovaAttack.cs
+++ Assets/Scripts/Player/Abilities/SuperNovaAttack.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SuperNovaAttack : Ability {
     public override void Use(Vector3 spawnPos) {
-        RaycastHit[] hits = Physics.SphereCastAll(spawnPos, 1f, transform.forward, m_Info.Range);
-        foreach (RaycastHit hit in hits) {
-            if (hit.collider.CompareTag("Enemy")) {
-                hit.collider.GetComponent<EnemyController>().DecreaseHealth(m_Info.Power);
+        var emitterShape = cc_PS.shape;
+        emitterShape.radius = m_Info.Range;
+
+        Collider[] hitColliders = Physics.OverlapSphere(spawnPos, m_Info.Range);
+        
+        foreach (var hit in hitColliders) {
+            if (hit.GetComponent<Collider>().CompareTag("Enemy")) {
+                hit.GetComponent<Collider>().GetComponent<EnemyController>().DecreaseHealth(m_Info.Power);
+                Debug.Log("Smack");
             }
         }
 
-        var emitterShape = cc_PS.shape;
-        emitterShape.length = m_Info.Range;
+        
+        // emitterShape.length = m_Info.Range;
         cc_PS.Play();
     }
 }
